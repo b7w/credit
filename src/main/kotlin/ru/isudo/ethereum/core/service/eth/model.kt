ee53f2eeb8d1c6001fe5390b6752c918136155e9
ikonovalov <konovalov84@gmail.com>
2016-10-14 00:30:59 +0300

    Transaction looks like Eth transaction now

--- src/main/kotlin/ru/isudo/ethereum/core/service/eth/model.kt
+++ src/main/kotlin/ru/isudo/ethereum/core/service/eth/model.kt
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Transaction(
-        val hash: String,
-        val from: String,
-        val to: String?,
-        val gas: Long,
-        val gasPrice: Long,
-        val value: Long,
-        val input: Long
+        val blockHash: String = "",
+        val blockNumber: String = "",
+        val from: String? = "",
+        val gas: String = "",
+        val gasPrice: String = "",
+        val hash: String = "",
+        val input: String = "",
+        val nonce: String = "",
+        val to: String = "",
+        val transactionIndex: String = "",
+        val value: String = ""
 )
